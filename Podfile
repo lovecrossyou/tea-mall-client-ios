879ac4b95ad05a689fa81079ea09c67a3235cd8a
love1234 <345841634@qq.com>
2019-01-04 10:32:05 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,2 @@
+target 'tea-mall-ios'
+pod 'AFNetworking', '~> 1.0'
---
+++ Pods/Target Support Files/Pods-tea-mall-ios/Pods-tea-mall-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tea_mall_ios : NSObject
+@end
+@implementation PodsDummy_Pods_tea_mall_ios
+@end
